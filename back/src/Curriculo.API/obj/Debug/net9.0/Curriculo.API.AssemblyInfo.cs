f7d33e287e2f78b0d31db0b35e4c34630a345271
TonW2025 <wellington_@hotmail.com>
2025-01-29 10:11:32 -0300

    Evewntos e Palestrantes
    Criação de componentes

--- back/src/Curriculo.API/obj/Debug/net9.0/Curriculo.API.AssemblyInfo.cs
+++ back/src/Curriculo.API/obj/Debug/net9.0/Curriculo.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Curriculo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd7fdacf3117ff49c4c6e0da4ac7c230d945fa79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7246320bf4db96b22890eec97c5c54cba11fec26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Curriculo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Curriculo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
